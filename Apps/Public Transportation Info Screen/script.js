8ac1d8c0206827ac1e47988008a970759e7496a8
Tuzbay <t.uzbayyelce@gmail.com>
2023-01-04 00:13:27 +0300

    1.Page added (Public Transportation Info Screen)

--- Apps/Public Transportation Info Screen/script.js
+++ Apps/Public Transportation Info Screen/script.js
@@ -1,18 +1,24 @@
 const doorOne = document.querySelector(".door-one");
 const doorTwo = document.querySelector(".door-two");
-const arrow = document.querySelector(".arrow i");
+const arrow = document.querySelector(".arrow img");
 const door = document.querySelector(".door");
 const text = document.querySelector(".text");
 const bostanci = document.querySelector(".screen1__location .bostanci");
-const arrowArriving = document.querySelector(".done-path i");
+const arrowArriving = document.querySelector(".done-path img");
 const screen = document.querySelector(".screen");
 const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+var styleElem = document.head.appendChild(document.createElement("style"));
+const firstdonePath = document.querySelector(".firstdonePath");
+const bostanci1 = document.querySelector(".screen2__location .bostanci1");
+const train = document.querySelector(".firstdonePath i");
+
 function openDoor() {
   doorOne.style.marginRight = "10rem";
   doorTwo.style.marginLeft = "10rem";
   door.style.position = "relative";
   arrow.style.position = "absolute";
-  arrow.style.bottom = "30rem";
+  arrow.style.bottom = "26rem";
   arrow.style.transition = ".4s ease all";
   doorOne.style.transition = ".3s ease all";
   doorTwo.style.transition = ".3s ease all";
@@ -23,7 +29,7 @@ function closeDoor() {
   doorTwo.style.marginLeft = "1rem";
   door.style.position = "relative";
   arrow.style.position = "absolute";
-  arrow.style.bottom = "13rem";
+  arrow.style.bottom = "8.5rem";
   arrow.style.transition = ".4s ease all";
   doorOne.style.transition = ".3s ease all";
   doorTwo.style.transition = ".3s ease all";
@@ -38,19 +44,34 @@ function textEng() {
 }
 
 function arriving1() {
-  bostanci.style.color = "yellow";
+  bostanci.style.color = "blue";
   arrowArriving.style.visibility = "visible";
-  bostanciTop.style.display = "hidden";
+  arrowArriving.style.transition = ".1s linear all";
+  styleElem.innerHTML =
+    "#bostanci:before {color: yellow; transition: .2s linear all;}";
 }
 
 function arriving2() {
-  bostanci.style.color = "green";
+  bostanci.style.color = "red";
   arrowArriving.style.visibility = "hidden";
+  arrowArriving.style.transition = ".1s linear all";
+  styleElem.innerHTML =
+    "#bostanci:before {color: green;transition: .2s linear all;}";
+}
+
+function page1() {
+  screen.style.display = "none";
+  screen1.style.display = "none";
+  screen2.style.display = "flex";
+  firstdonePath.style.width = "130px";
+  setInterval(first1arriving, 500);
+  setInterval(first2arriving, 1000);
 }
 
 function page2() {
   screen.style.display = "none";
   screen1.style.display = "flex";
+  screen2.style.display = "none";
   setInterval(arriving1, 500);
   setInterval(arriving2, 1000);
 }
@@ -58,10 +79,23 @@ function page2() {
 function page3() {
   screen.style.display = "block";
   screen1.style.display = "none";
+  screen2.style.display = "none";
   setInterval(openDoor, 500);
   setInterval(closeDoor, 1000);
   setInterval(textTurk, 500);
   setInterval(textEng, 1000);
 }
 
-page2();
+function first1arriving() {
+  styleElem.innerHTML =
+    "#bostanci1:before {color: green;transition: .3s ease-in all; }";
+  train.style.color = "red";
+}
+
+function first2arriving() {
+  styleElem.innerHTML =
+    "#bostanci1:before {color: rgb(248, 244, 244);transition: .3s ease-out all;}";
+  train.style.color = "yellow";
+}
+
+page1();
