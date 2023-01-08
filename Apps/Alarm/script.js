e191e2e11e2513d46a9298c827480bd36251cbf7
Tuzbay <t.uzbayyelce@gmail.com>
2023-01-08 17:37:20 +0300

    Alarm and Calendar Completed

--- Apps/Alarm/script.js
+++ Apps/Alarm/script.js
@@ -1,55 +1,48 @@
-const currentTime = document.querySelector("h1");
-content = document.querySelector(".content");
-selectMenu = document.querySelectorAll("select");
-setAlarmBtn = document.querySelector("button");
+const currentTime = document.querySelector("h1"),
+  content = document.querySelector(".content"),
+  selectMenu = document.querySelectorAll("select"),
+  setAlarmBtn = document.querySelector("button"),
+  img = document.querySelector("img");
 
-let alarmTime,
-  isAlarmSet = false;
-ringtone = new Audio("./Alarm-clock-bell-ringing-sound-effect.mp3");
+let alarmTime;
+let isAlarmSet = false;
+ringtone = new Audio("./Alarm-clock-bell-ringing-sound-effect.mp3"); // * Alarm çaldığında duyulacak zil sesi
 
-for (let i = 12; i > 0; i--) {
-  i = i < 10 ? "0" + i : i;
-  let option = `<option value="${i}"> ${i} </option>`;
-  selectMenu[0].firstElementChild.insertAdjacentHTML("afterend", option);
+for (let i = 24; i > 0; i--) {
+  i = i < 10 ? `0${i}` : i;
+  let option = `<option value="${i}">${i}</option>`;
+  selectMenu[0].firstElementChild.insertAdjacentHTML("afterend", option); // * Saat bölümünde yer alan 24 sayı
 }
 
-for (let i = 59; i > 0; i--) {
-  i = i < 10 ? "0" + i : i;
-  let option = `<option value="${i}"> ${i} </option>`;
-  selectMenu[1].firstElementChild.insertAdjacentHTML("afterend", option);
-}
-
-for (let i = 2; i > 0; i--) {
-  let ampm = i == 1 ? "AM" : "PM";
-  let option = `<option value="${ampm}"> ${ampm} </option>`;
-  selectMenu[2].firstElementChild.insertAdjacentHTML("afterend", option);
+for (let i = 59; i >= 0; i--) {
+  i = i < 10 ? `0${i}` : i;
+  let option = `<option value="${i}">${i}</option>`;
+  selectMenu[1].firstElementChild.insertAdjacentHTML("afterend", option); // * Dakika bölümünde yer alan 60 sayı
 }
 
 setInterval(() => {
   let date = new Date(),
     h = date.getHours(),
     m = date.getMinutes(),
-    s = date.getSeconds(),
-    ampm = "AM";
+    s = date.getSeconds();
 
-  if (h >= 12) {
-    h = h - 12;
-    ampm = "PM";
-  }
-
-  h = h == 0 ? (h = 12) : h;
-
-  h = h < 10 ? "0" + h : h;
+  h = h == 0 ? (h = 24) : h;
+  h = h < 10 ? "0" + h : h; // * Bu satırlar rakamların önüne 0 koyar.
   m = m < 10 ? "0" + m : m;
   s = s < 10 ? "0" + s : s;
+  currentTime.innerText = `${h}:${m}:${s} `; // * Mevcut saat bu kod ile ekranda gösterilmektedir.
 
-  currentTime.innerText = `${h}:${m}:${s} ${ampm}`;
-
-  if (alarmTime == `${h}:${m} ${ampm}`) {
+  // * Alarm saati ile local saat eşleştiğinde bu if bloğu çalışmaktadır.
+  if (alarmTime === `${h}:${m}`) {
     ringtone.play();
     ringtone.loop = true;
+    img.src =
+      "./alarm-clock-icon-alarm-clock-that-sounds-loudly-morning-wake-up-from-bed_68708-698.webp";
+    img.style.animation = "clock 10s linear";
   }
-}, 1000);
+});
+
+// * setAlarm fonksiyonunda başlangıçta isAlarmSet false değerindedir. O yüzden aşağıdaki komutlar çalışır. Alarm kurulduktan sonra isAlarmSet true değerine sahiptir ve bir daha tıklandığında if bloğu içindeki komutları çalıştırır.
 
 function setAlarm() {
   if (isAlarmSet) {
@@ -59,18 +52,13 @@ function setAlarm() {
     setAlarmBtn.innerText = "Set Alarm";
     return (isAlarmSet = false);
   }
-
-  let time = `${selectMenu[0].value}:${selectMenu[1].value} ${selectMenu[2].value}`;
-
-  if (
-    time.includes("Hour") ||
-    time.includes("Minute") ||
-    time.includes("AM/PM")
-  ) {
+  // * Aşağıdaki komut satırı ile birlikte alarm bölümünde saat ve dakika alanında bir değer seçilmemişse uyarı verir.
+  let time = `${selectMenu[0].value}:${selectMenu[1].value}`;
+  if (time.includes("Hour") || time.includes("Minute")) {
     return alert("Please, select a valid time to set Alarm!");
   }
-  isAlarmSet = true;
   alarmTime = time;
+  isAlarmSet = true;
   content.classList.add("disable");
   setAlarmBtn.innerText = "Clear Alarm";
 }
--- Apps/Calendar/script.js
+++ Apps/Calendar/script.js
@@ -1,10 +1,10 @@
 const currentDate = document.querySelector(".current-date");
 daysTag = document.querySelector(".days");
 prevNextIcon = document.querySelectorAll(".icons span");
-
+// Aşağıdaki satırlarda yılı ve ayı belirliyoruz. Sonrasında tüm ayları bir array içine alıyoruz.
 let date = new Date();
-currYear = date.getFullYear();
-currMonth = date.getMonth();
+let currYear = date.getFullYear();
+let currMonth = date.getMonth();
 
 const months = [
   "January",
@@ -21,17 +21,20 @@ const months = [
   "December",
 ];
 
+//
+
 const renderCalendar = () => {
-  let firstDayofMonth = new Date(currYear, currMonth, 1).getDay();
-  let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate();
-  let lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay();
-  let lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
+  let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(); // ! Burada bulunduğumuz yılın 1. gününü alıyoruz. Sonuç 0 olarak dönüyor. Yani Pazar günü...
+  let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(); // ! Son değer 0 olduğu için 2. ayın bir gün öncesini alıyoruz. Yani geçtiğimiz ayın son gününü bulmuş oluyoruz.
+  let lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(); // ! Bulduğumuz son günün hangi günde olduğunu buluyoruz.
+  let lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // ! Bir önceki ayın son gününü buluyoruz.
   let liTag = "";
 
+  // ! Aşağıdaki kod satırında bir önceki ayın son günleri yazılır.
   for (let i = firstDayofMonth; i > 0; i--) {
     liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
   }
-
+  // ! Bulunduğumuz günün işaretlenmesi için kontrolleri aşağıdaki kod satırı ile yapıyoruz.
   for (let i = 1; i <= lastDateofMonth; i++) {
     let isToday =
       i === date.getDate() &&
@@ -41,21 +44,22 @@ const renderCalendar = () => {
         : "";
     liTag += `<li class="${isToday}">${i}</li>`;
   }
-
+  // ! Aşağıdaki kod satırında bir sonraki ayın ilk günleri yazılır.
   for (let i = lastDayofMonth; i < 6; i++) {
     liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
   }
-
+  // ! Aşağıdaki kod satırında takvimin sol üst köşesindeki kısmın kodu vardır.
   currentDate.innerText = ` ${months[currMonth]} ${currYear}`;
   daysTag.innerHTML = liTag;
 };
 
 renderCalendar();
-
+// ! Aşağıdaki kod satırlarında takvimin sağ üstündeki iconlar aktif edilmiştir.
 prevNextIcon.forEach((icon) => {
   icon.addEventListener("click", () => {
     currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
 
+    // ! Aşağıdaki kod satırında takvimin sol üst tarafı butonlara basıldıkça güncellenmektedir.
     if (currMonth < 0 || currMonth > 11) {
       date = new Date(currYear, currMonth);
       currYear = date.getFullYear();
